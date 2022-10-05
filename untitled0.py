e2c8899bb1229900196e336c3ca9b8a55ec09d40
“Jean6-971” <75419220+Jean6-971@users.noreply.github.com>
2022-10-05 10:21:23 +0200

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -23,18 +23,9 @@ model = cluster.KMeans(n_cluster=k, init='k-means++')
 model.fit(datanp)
 tps2 = time.time()
 labels = model.labels_
-
 iteration = model.n_iter_
 
- 
-
-‘k, init =’k—means++’)
-
 plt.scatter(f0, f1, c=labels, s=8)
-
-plt.title(*Donnees apres clustering Kmeans")
-
+plt.title("Donnees apres clustering Kmeans")
 plt .show ()
-
-print ("nb clusters =",k,", nb iter =',iteration, ",
-runtime = ", round((tps2 — tpsl)*1000,2),"ms")
+print ("nb clusters =",k,", nb iter =",iteration, ",runtime = ", round((tps2-tpsl)*1000,2),"ms")
