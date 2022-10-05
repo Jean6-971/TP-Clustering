c4f437938276db7da5e1b92e13395fa11d160210
guerbett <guerbett@etud.insa-toulouse.fr>
2022-10-05 12:15:50 +0200

    bm

--- K-Means.py
+++ K-Means.py
@@ -14,7 +14,7 @@ from sklearn.metrics import silhouette_samples, silhouette_score, davies_bouldin
 # numero de cluster. On retire cette information
 
 path = "./artificial/"
-databrut = arff.loadarff(open(path+"R15.arff", "r"))
+databrut = arff.loadarff(open(path+"smile1.arff", "r"))
 datanp = [[x[0],x[1]] for x in databrut [0]]
 
 # Affichage en 2D
@@ -34,32 +34,40 @@ davies_bouldin = []
 calinski_harabasz = []
 
 # Détermination du nombre de clusters par coefficient de silhouette
-range_clust = range(2,50)
+range_clust = range(2,10)
 for n_clusters in range_clust : 
-    #print ("Appel KMeans pour une valeur fixee de k ")
-    #tps1 = time.time()
+    print ("Appel KMeans pour une valeur fixee de k ")
+    tps1 = time.time()
     model = cluster.KMeans(n_clusters=n_clusters, init='k-means++')
     model.fit(datanp)
-    #tps2 = time.time()
+    tps2 = time.time()
     labels = model.labels_
-    
+    tp_kmeans = tps2-tps1
     # The silhouette_score gives the average value for all the samples.
     # This gives a perspective into the density and separation of the formed
     # clusters
+    tps1 = time.time()
     silhouette.append(silhouette_score(datanp, labels))
+    tps2 = time.time()
+    tp_silhouette = tps2-tps1
+    tps1 = time.time()
     davies_bouldin.append(davies_bouldin_score(datanp, labels))
+    tps2 = time.time()
+    tp_bouldin = tps2-tps1
+    tps1 = time.time()
     calinski_harabasz.append(calinski_harabasz_score(datanp, labels)/7000)
+    tps2 = time.time()
+    tp_calinski = tps2-tps1
+    iteration = model.n_iter_
+    print ("nb clusters =",n_clusters,", nb iter =",iteration, 
+           ",runtime kmeans = ", round((tp_kmeans)*1000,2),"ms",
+           ",runtime silhouette = ", round((tp_silhouette)*1000,2),"ms",
+           ",runtime bouldin = ", round((tp_bouldin)*1000,2),"ms", 
+           ",runtime calinski = ", round((tp_calinski)*1000,2),"ms",)
 print(silhouette)
 plt.plot(range_clust,silhouette,'r')
 plt.plot(range_clust,davies_bouldin,'g')
 plt.plot(range_clust,calinski_harabasz,'b')
 
 plt.show() 
-# =============================================================================
-#     iteration = model.n_iter_
-#     
-#     plt.scatter(f0, f1, c=labels, s=8)
-#     plt.title("Donnees apres clustering Kmeans")
-#     plt .show ()
-#     print ("nb clusters =",n_clusters,", nb iter =",iteration, ",runtime = ", round((tps2-tps1)*1000,2),"ms")
-# =============================================================================
+
