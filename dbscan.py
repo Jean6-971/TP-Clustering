8824e74226ebe480487d88df7863ed8de35fd224
“Jean6-971” <75419220+Jean6-971@users.noreply.github.com>
2022-10-10 11:36:52 +0200

    Update dbscan.py

--- dbscan.py
+++ dbscan.py
@@ -5,13 +5,15 @@ Created on Mon Oct 10 11:03:24 2022
 @author: jeanv
 """
 
-import scipy . cluster . hierarchy as shc
+import scipy.cluster.hierarchy as shc
 import matplotlib.pyplot as plt
 from scipy.io import arff
 import time
 from sklearn import cluster
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
+from sklearn.metrics import silhouette_samples, silhouette_score, davies_bouldin_score, calinski_harabasz_score
+
 
 # Donnees dans datanp
 
@@ -38,15 +40,48 @@ plt.plot(trie);
 plt.show()
 
 
+silhouette = []
+davies_bouldin = []
+calinski_harabasz = []
+range_min_samples = range(1,20)
+for min_samples in range_min_samples : 
+    tps1 = time.time()
+    model = cluster.DBSCAN(eps= 0.3, min_samples= min_samples)
+    model = model.fit(datanp)
+    tps2 = time.time()
+    labels = model.labels_
+    tps_dbscan = tps2 - tps1
+    
+    tps1 = time.time()
+    silhouette.append(silhouette_score(datanp, labels))
+    tps2 = time.time()
+    tp_silhouette = tps2 - tps1
+    tps1 = time.time()
+    davies_bouldin.append(davies_bouldin_score(datanp, labels))
+    tps2 = time.time()
+    tp_bouldin = tps2 - tps1
+    tps1 = time.time()
+    calinski_harabasz.append(calinski_harabasz_score(datanp, labels)/500)
+    tps2 = time.time()
+    tp_calinski = tps2 - tps1
+    print ("min sample =",min_samples, 
+           ",runtime dbscan = ", round((tps_dbscan)*1000,2),"ms",
+           ",runtime silhouette = ", round((tp_silhouette)*1000,2),"ms",
+           ",runtime bouldin = ", round((tp_bouldin)*1000,2),"ms", 
+           ",runtime calinski = ", round((tp_calinski)*1000,2),"ms",)
 
-tps1 = time.time()
-model = cluster.DBSCAN(eps= 0.7, min_samples= 20)
-model = model.fit(datanp)
-tps2 = time.time()
-labels = model.labels_
+print(silhouette)
+plt.plot(range_min_samples,silhouette,'r')
+plt.plot(range_min_samples,davies_bouldin,'g')
+plt.plot(range_min_samples,calinski_harabasz,'b')
+plt.legend(["silhouette", "davies_bouldin", "calinski_harabasz"])
+
+plt.show() 
 
 # Affichage clustering
-plt.scatter(f0, f1, c=labels, s=8)
-plt.title(" Resultat du clustering ")
-plt.show()
-print (" runtime = ", round ((tps2 - tps1)*1000, 2), "ms")
+# =============================================================================
+# plt.scatter(f0, f1, c=labels, s=8)
+# plt.title(" Resultat du clustering ")
+# plt.show()
+# print (" runtime = ", round ((tps2 - tps1)*1000, 2), "ms")
+# =============================================================================
