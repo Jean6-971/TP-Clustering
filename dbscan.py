4a2408af06794bea6a57564332471c4328785954
“Jean6-971” <75419220+Jean6-971@users.noreply.github.com>
2022-10-10 12:13:37 +0200

    Update dbscan.py

--- dbscan.py
+++ dbscan.py
@@ -18,70 +18,39 @@ from sklearn.metrics import silhouette_samples, silhouette_score, davies_bouldin
 # Donnees dans datanp
 
 path = "./artificial/"
-databrut = arff.loadarff(open(path+"R15.arff", "r"))
+databrut = arff.loadarff(open(path+"smile1.arff", "r"))
 datanp = [[x[0],x[1]] for x in databrut [0]]
 
 f0 = [f[0] for f in datanp]
 f1 = [f[1] for f in datanp]
 
+# range_min_samples = range(2,50)
+# for min_samples in range_min_samples :
+#     # Distances k plus proches voisins
+#     # Donnees dans X
+#     k=min_samples
+#     neigh = NearestNeighbors(n_neighbors= k)
+#     neigh.fit(datanp)
+#     distances, indices = neigh.kneighbors(datanp)
 
-# Distances k plus proches voisins
-# Donnees dans X
-k=5
-neigh = NearestNeighbors(n_neighbors= k)
-neigh.fit(datanp)
-distances, indices = neigh.kneighbors(datanp)
+#     # retirer le point " origine "
+#     newDistances = np.asarray([np.average(distances[i][1:]) for i in range (0, distances.shape[0])])
+#     trie = np.sort(newDistances)
+#     title=" Plus proches voisins (", min_samples, ")"
+#     plt.title(title)
+#     plt.plot(trie);
+#     plt.show()
 
-# retirer le point " origine "
-newDistances = np.asarray([np.average(distances[i][1:]) for i in range (0, distances.shape[0])])
-trie = np.sort(newDistances)
-plt.title(" Plus proches voisins (5)")
-plt.plot(trie);
-plt.show()
-
-
-silhouette = []
-davies_bouldin = []
-calinski_harabasz = []
-range_min_samples = range(1,20)
-for min_samples in range_min_samples : 
-    tps1 = time.time()
-    model = cluster.DBSCAN(eps= 0.3, min_samples= min_samples)
-    model = model.fit(datanp)
-    tps2 = time.time()
-    labels = model.labels_
-    tps_dbscan = tps2 - tps1
-    
-    tps1 = time.time()
-    silhouette.append(silhouette_score(datanp, labels))
-    tps2 = time.time()
-    tp_silhouette = tps2 - tps1
-    tps1 = time.time()
-    davies_bouldin.append(davies_bouldin_score(datanp, labels))
-    tps2 = time.time()
-    tp_bouldin = tps2 - tps1
-    tps1 = time.time()
-    calinski_harabasz.append(calinski_harabasz_score(datanp, labels)/500)
-    tps2 = time.time()
-    tp_calinski = tps2 - tps1
-    print ("min sample =",min_samples, 
-           ",runtime dbscan = ", round((tps_dbscan)*1000,2),"ms",
-           ",runtime silhouette = ", round((tp_silhouette)*1000,2),"ms",
-           ",runtime bouldin = ", round((tp_bouldin)*1000,2),"ms", 
-           ",runtime calinski = ", round((tp_calinski)*1000,2),"ms",)
-
-print(silhouette)
-plt.plot(range_min_samples,silhouette,'r')
-plt.plot(range_min_samples,davies_bouldin,'g')
-plt.plot(range_min_samples,calinski_harabasz,'b')
-plt.legend(["silhouette", "davies_bouldin", "calinski_harabasz"])
-
-plt.show() 
+tps1 = time.time()
+model = cluster.DBSCAN(eps= 0.032, min_samples= 8)
+model = model.fit(datanp)
+tps2 = time.time()
+labels = model.labels_
 
 # Affichage clustering
 # =============================================================================
-# plt.scatter(f0, f1, c=labels, s=8)
-# plt.title(" Resultat du clustering ")
-# plt.show()
-# print (" runtime = ", round ((tps2 - tps1)*1000, 2), "ms")
+plt.scatter(f0, f1, c=labels, s=8)
+plt.title(" Resultat du clustering ")
+plt.show()
+print (" runtime = ", round ((tps2 - tps1)*1000, 2), "ms")
 # =============================================================================
